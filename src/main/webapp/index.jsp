2923a7d4ba80791fac0d231bc4147833dd03ecbe
Zyuzin Anton <anton.zyuzin@pearlchain.net>
2018-01-22 17:12:09 +0700

    add react js

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
+<spring:url value="/webjars/app-bundle.js" var="appJs" />
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <body>
-        <h1>Hello, World!</h1>
+        <div id="react"></div>
+
+        <script src="${appJs}"></script>
     </body>
 </html>
---
+++ src/main/webapp/resources/static/webpack.config.js
@@ -0,0 +1,40 @@
+var packageJSON = require('./package.json');
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    devtool: 'source-map',
+    entry: './index.js',
+    output: {
+        path: path.join(__dirname, 'webjars'),
+        filename: 'app-bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            debug: true}),
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("development")
+            }
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+	       }
+        ]
+    },
+    devServer: {
+        noInfo: false,
+        quiet: false,
+        lazy: false,
+        watchOptions: {
+            poll: true
+       }
+    }
+}
