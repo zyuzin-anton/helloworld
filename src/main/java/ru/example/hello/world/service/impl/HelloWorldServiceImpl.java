c33a3d81ca23ab99c9b69dac6ca8af78d94530c5
aozyuzin <Zyuzin.A.Ole@sberbank.ru>
2020-12-07 22:16:05 +0700

    added not found error

--- src/main/java/ru/example/hello/world/service/impl/HelloWorldServiceImpl.java
+++ src/main/java/ru/example/hello/world/service/impl/HelloWorldServiceImpl.java
@@ -1,8 +1,10 @@
 package ru.example.hello.world.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 import ru.example.hello.world.dto.HelloWorldDto;
 import ru.example.hello.world.entity.Hello;
@@ -21,6 +23,9 @@ public class HelloWorldServiceImpl extends BaseServiceImpl<Hello, Long> implemen
 
     @Transactional(readOnly = true)
     public Mono<HelloWorldDto> find(Long id) {
-        return helloWorldRepository.findById(id).map(entity -> helloWorldMapper.toDto(entity));
+        return helloWorldRepository
+                .findById(id)
+                .map(entity -> helloWorldMapper.toDto(entity))
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
 }
