5f1cd8811f3c4ea1243496a4e30568ba082f4dd3
Зюзин Антон Олегович <Zyuzin.A.Ole@sberbank.ru>
2021-02-12 18:47:52 +0700

    added unit tests

---
+++ src/test/java/ru/example/hello/world/service/impl/HelloWorldServiceTest.java
@@ -0,0 +1,62 @@
+package ru.example.hello.world.service.impl;
+
+import lombok.val;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
+import ru.example.hello.world.dto.HelloWorldDto;
+import ru.example.hello.world.entity.Hello;
+import ru.example.hello.world.mapper.HelloWorldMapper;
+import ru.example.hello.world.repository.HelloWorldRepository;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HelloWorldServiceTest {
+
+    @Mock
+    private HelloWorldRepository helloWorldRepository;
+
+    @Mock
+    private HelloWorldMapper helloWorldMapper;
+
+    @InjectMocks
+    private HelloWorldServiceImpl helloWorldService;
+
+    private Long existedId = 1L;
+    private Long nonExistedId = 2L;
+    private String description = "Hello, World!";
+
+    @Before
+    public void init() {
+        val helloMock = Mono.just(Mockito.mock(Hello.class));
+        when(helloWorldRepository.findById(nonExistedId)).thenReturn(Mono.empty());
+        when(helloWorldRepository.findById(existedId)).thenReturn(helloMock);
+
+        val helloWorldDtoMock = Mockito.mock(HelloWorldDto.class);
+        when(helloWorldDtoMock.getDescription()).thenReturn(description);
+
+        when(helloWorldMapper.toDto(helloMock.block())).thenReturn(helloWorldDtoMock);
+    }
+
+    @Test
+    public void successFind() {
+        val result = helloWorldService.find(existedId);
+
+        assertEquals(result.block().getDescription(), description);
+    }
+
+    @Test(expected = ResponseStatusException.class)
+    public void failFind() {
+        val result = helloWorldService.find(nonExistedId);
+
+        result.block();
+    }
+}
